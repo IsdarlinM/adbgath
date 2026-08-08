900d16057b7769b52fb3ca8f9d22925f9b234d10
isdarlinm <82850219+IsdarlinM@users.noreply.github.com>
2026-08-08 15:57:47 -0400

    release: bump ADB-Gath to 3.7.0

--- src/adbgath/__init__.py
+++ src/adbgath/__init__.py
@@ -1,8 +1,8 @@
 """adbgath package."""
 
-__version__ = "3.6.0"
+__version__ = "3.7.0"
 
-from .compat360 import apply as _apply_360
+from .compat370 import apply as _apply_370
 
-_apply_360()
-del _apply_360
+_apply_370()
+del _apply_370
