6ecc2e2a29dcdd93b322d3058fd933853e72544d
isdarlinm <82850219+IsdarlinM@users.noreply.github.com>
2026-08-09 01:02:53 -0400

    chore(release): bump ADB-Gath to 3.7.2

--- src/adbgath/__init__.py
+++ src/adbgath/__init__.py
@@ -1,8 +1,8 @@
 """adbgath package."""
 
-__version__ = "3.7.1"
+__version__ = "3.7.2"
 
-from .compat371 import apply as _apply_371
+from .compat372 import apply as _apply_372
 
-_apply_371()
-del _apply_371
+_apply_372()
+del _apply_372
